211eb1a1c94a646004bdf9017729554b8e4a2b5f
sameer-kumar04 <whoissameer04@gmail.com>
2025-08-29 17:37:42 +0530

    New Line Added to the java file

--- First.java
+++ First.java
@@ -2,8 +2,7 @@ package LocalRepo;
 
 public class First{
     public static void main(String[] args) {
-        for(int i = 0; i < 5; i++) {
-            System.out.println("Hello World");
-        }
+        System.out.println("Hello World");
+        System.out.println("Good Boy");
     }
 }
